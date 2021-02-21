4429a9a9bd9491d573f580adbdf263b25402aeec
Geidson Benicio Coelho <geidsonc@gmail.com>
2021-02-20 23:53:28 -0300

    Adiciona migraçao e classe User.

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type',
     ];
 
     /**
@@ -38,6 +39,5 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
